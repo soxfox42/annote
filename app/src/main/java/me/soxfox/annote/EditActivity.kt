a9899c44626601e2b30d9a6ba0bd1d82ebad8adb
Finn Coffey <finn.coffey@gmail.com>
2022-10-23 14:56:22 +1100

    Add attribute editing

--- app/src/main/java/me/soxfox/annote/EditActivity.kt
+++ app/src/main/java/me/soxfox/annote/EditActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TableLayout
+import android.widget.TableRow
 import androidx.activity.addCallback
 import androidx.core.widget.addTextChangedListener
 import org.dizitart.no2.Document
@@ -19,6 +21,9 @@ class EditActivity : AppCompatActivity() {
     private val content by lazy { findViewById<EditText>(R.id.content) }
     private val toggleAttrs by lazy { findViewById<ImageButton>(R.id.toggle_attrs) }
     private val attrs by lazy { findViewById<TableLayout>(R.id.attrs) }
+    private val addAttr by lazy { findViewById<Button>(R.id.add_attr) }
+
+    private val fields = mutableListOf<TableRow>()
 
     private lateinit var notes: NitriteCollection
     private lateinit var note: Document
@@ -60,6 +65,18 @@ class EditActivity : AppCompatActivity() {
             } else {
                 attrs.visibility = View.GONE
             }
+        }
+
+        // Listen for create attribute
+        addAttr.setOnClickListener {
+            val row = layoutInflater.inflate(R.layout.row_attr, attrs, false)
+            val delete = row.findViewById<ImageButton>(R.id.delete)
+            delete.setOnClickListener {
+                attrs.removeView(row)
+                fields.remove(row)
+            }
+            attrs.addView(row, attrs.childCount - 1)
+            fields.add(row as TableRow)
         }
 
         // Listen for back press
